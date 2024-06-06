85a9244cf95471dd8b7b4f0aaefc6c6c9e540f05
Lucas_Martins <lucaspmartins14@gmail.com>
2024-06-06 17:08:10 -0300

    Versão de IB2

--- Testes/LED_driver/LED_driver.ino
+++ Testes/LED_driver/LED_driver.ino
@@ -1,69 +1,50 @@
 const int LED1_PIN = 2;
 const int LED2_PIN = 4;
-const int ANALOG_PIN = 34;
+const int ANALOG_PIN = 36;
 
-const int LED1_CHANNEL = 0;
-const int LED2_CHANNEL = 1;
-const int PWM_FREQ = 5000;
-const int PWM_RESOLUTION = 12; // 12 bits
+const int INTERVALO_LED1 = 1000;
+const int INTERVALO_LED2 = 500;
 
-// ajustáveis
-int LED1_POWER = 2048; // 50% de potência (0-4095 para resolução de 12 bits)
-int LED2_POWER = 3072; // 75% de potência
-int LED1_INTERVAL = 1000; // Intervalo de 1 segundo
-int LED2_INTERVAL = 1500; // Intervalo de 1.5 segundos
+int frequenciaLeitura = 200;
 
-int ANALOG_READ_FREQ = 10; // 10 leitura em um segundo
-
-void blinkLED1(void *parameter) {
-  while (true) {
-    // Liga o LED1
-    ledcWrite(LED1_CHANNEL, LED1_POWER);
-    vTaskDelay(pdMS_TO_TICKS(LED1_INTERVAL));
-    
-    // Desliga o LED1
-    ledcWrite(LED1_CHANNEL, 0);
-    vTaskDelay(pdMS_TO_TICKS(LED1_INTERVAL));
-  }
-}
-
-void blinkLED2(void *parameter) {
-  while (true) {
-    // Liga o LED2
-    ledcWrite(LED2_CHANNEL, LED2_POWER);
-    vTaskDelay(pdMS_TO_TICKS(LED2_INTERVAL));
-    
-    // Desliga o LED2
-    ledcWrite(LED2_CHANNEL, 0);
-    vTaskDelay(pdMS_TO_TICKS(LED2_INTERVAL));
-  }
-}
-
-void readAnalog(void *parameter) {
-  const TickType_t xFrequency = pdMS_TO_TICKS(1000 / ANALOG_READ_FREQ);
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  while (true) {
-    int analogValue = analogRead(ANALOG_PIN);
-    Serial.println(analogValue);
-    
-    vTaskDelayUntil(&xLastWakeTime, xFrequency);
-  }
-}
+TaskHandle_t Task1Handle;
+TaskHandle_t Task2Handle;
+TaskHandle_t AnalogReadHandle;
 
 void setup() {
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
   Serial.begin(115200);
-
-  // Configuração do PWM para os LED
-  ledcSetup(LED1_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcSetup(LED2_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcAttachPin(LED1_PIN, LED1_CHANNEL);
-  ledcAttachPin(LED2_PIN, LED2_CHANNEL);
-
-  // Criação das tarefas do FreeRTOS
-  xTaskCreate(blinkLED1, "Blink LED1", 1024, NULL, 1, NULL);
-  xTaskCreate(blinkLED2, "Blink LED2", 1024, NULL, 1, NULL);
-  xTaskCreate(readAnalog, "Read Analog", 1024, NULL, 1, NULL);
+  xTaskCreatePinnedToCore(Task1, "Task1", 1000, NULL, 1, &Task1Handle, 0);
+  xTaskCreatePinnedToCore(Task2, "Task2", 1000, NULL, 1, &Task2Handle, 1);
+  xTaskCreatePinnedToCore(AnalogReadTask, "AnalogReadTask", 1000, NULL, 1, &AnalogReadHandle, 1);
 }
 
 void loop() {
+}
+
+void Task1(void *pvParameters) {
+  while(1) {
+    digitalWrite(LED1_PIN, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(INTERVALO_LED1));
+    digitalWrite(LED1_PIN, LOW);
+    vTaskDelay(pdMS_TO_TICKS(INTERVALO_LED1));
+  }
+}
+
+void Task2(void *pvParameters) {
+  while(1) {
+    digitalWrite(LED2_PIN, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(INTERVALO_LED2));
+    digitalWrite(LED2_PIN, LOW);
+    vTaskDelay(pdMS_TO_TICKS(INTERVALO_LED2));
+  }
+}
+
+void AnalogReadTask(void *pvParameters) {
+  while(1) {
+    int valorAnalogico = analogRead(ANALOG_PIN);
+    Serial.println(valorAnalogico);
+    vTaskDelay(pdMS_TO_TICKS(frequenciaLeitura));
+  }
 }
