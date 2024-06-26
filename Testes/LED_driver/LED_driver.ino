013bcd84d5c4751f6d48123e4762a9ff64dbf395
Lucas_Martins <lucaspmartins14@gmail.com>
2024-06-26 18:57:16 -0300

    Teste com LED_RGB

--- Testes/LED_driver/LED_driver.ino
+++ Testes/LED_driver/LED_driver.ino
@@ -1,36 +1,24 @@
-#include <Arduino.h>
+#define LED_RED_PIN 34
+#define LED_GREEN_PIN 32
+#define LED_BLUE_PIN 25
+#define LED_IR_PIN 13
 
-#define LED1_PIN 14
-#define LED2_PIN 12
-#define PHOTODIODE_PIN 34
+#define PHOTODIODE_PIN 26
 
 volatile uint8_t ledState = 0;
 
 void setup() {
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  pinMode(LED_IR_PIN, OUTPUT);
+
   pinMode(PHOTODIODE_PIN, INPUT);
-
-  xTaskCreatePinnedToCore(
-    toggleLEDs,         
-    "Toggle LEDs",      
-    1024,              
-    NULL,              
-    1,                  
-    NULL,              
-    1                   
-  );
-
-  xTaskCreatePinnedToCore(
-    readPhotodiode,   
-    "Read Photodiode",  
-    1024,               
-    NULL,               
-    1,                 
-    NULL,               
-    1                  
-  );
+  Serial.begin(9600);  // Inicialize a comunicação serial
+  delay(2000); // Espera para a inicialização da comunicação serial
+  
+  xTaskCreatePinnedToCore(toggleLEDs, "Toggle LEDs", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(readPhotodiode, "Read Photodiode", 1024, NULL, 1, NULL, 1);
 }
 
 void loop() {
@@ -38,12 +26,39 @@ void loop() {
 
 void toggleLEDs(void *parameter) {
   while (true) {
-    ledState = (ledState + 1) & 0x03; 
+    switch (ledState) {
+      case 0:
+        digitalWrite(LED_RED_PIN, HIGH);
+        digitalWrite(LED_GREEN_PIN, LOW);
+        digitalWrite(LED_BLUE_PIN, LOW);
+        digitalWrite(LED_IR_PIN, LOW);
+        Serial.println("LED Vermelho Ligado");
+        break;
+      case 1:
+        digitalWrite(LED_RED_PIN, LOW);
+        digitalWrite(LED_GREEN_PIN, HIGH);
+        digitalWrite(LED_BLUE_PIN, LOW);
+        digitalWrite(LED_IR_PIN, LOW);
+        Serial.println("LED Verde Ligado");
+        break;
+      case 2:
+        digitalWrite(LED_RED_PIN, LOW);
+        digitalWrite(LED_GREEN_PIN, LOW);
+        digitalWrite(LED_BLUE_PIN, HIGH);
+        digitalWrite(LED_IR_PIN, LOW);
+        Serial.println("LED Azul Ligado");
+        break;
+      case 3:
+        digitalWrite(LED_RED_PIN, LOW);
+        digitalWrite(LED_GREEN_PIN, LOW);
+        digitalWrite(LED_BLUE_PIN, LOW);
+        digitalWrite(LED_IR_PIN, HIGH);
+        Serial.println("LED IR Ligado");
+        break;
+    }
+    ledState = (ledState + 1) % 4; // Alterna entre 0, 1, 2 e 3
 
-    digitalWrite(LED1_PIN, (ledState == 0) ? HIGH : LOW);
-    digitalWrite(LED2_PIN, (ledState == 2) ? HIGH : LOW);
-
-    vTaskDelay(1200 / portTICK_PERIOD_MS);
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -52,6 +67,6 @@ void readPhotodiode(void *parameter) {
     int sensorValue = analogRead(PHOTODIODE_PIN);
     Serial.println(sensorValue);
     
-    vTaskDelay(400 / portTICK_PERIOD_MS);
-  }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
 }
